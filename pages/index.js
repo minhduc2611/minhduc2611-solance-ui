8852085a980096497f566d50da4b24618030ce5f
DucLeMinh <minhduc261298@gmail.com>
2022-12-15 15:22:13 +0700

    Modify base component

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,7 @@
-import styles from "../styles/Home.module.css";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { useAuthenticationContext } from "../context/AuthenticationContextProvider";
-import { useRouter } from "next/router";
+import ConnectWallet from "./connect-wallet";
 
 export default function Home() {
-  const { connected } = useAuthenticationContext();
-  const router = useRouter()
-  if (connected) {
-    router.push('home', undefined, { shallow: true })
-  }
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <div className="loginContainer">
-          <WalletMultiButton />
-        </div>
-      </main>
-    </div>
+    <ConnectWallet />
   );
 }
